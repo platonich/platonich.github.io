8666c6fdbe1764cbf0efd2c209ca1fca9a1227c2
Arsen Bespalov <me@arsen.pw>
2015-09-05 16:44:36 +0800

    update site

--- js/custom.js
+++ js/custom.js
@@ -4,6 +4,65 @@
 
     $(document).ready(function() {
 
+        /* Countdown timer
+        ==================================================================================== */
+        var TMR = 0, birthday = '23.07.2015 11:30:00';
+        setInterval(function() {
+            var a = birthday.split(' '),
+                b = a[0].split('.'),
+                c = a[1].split(':'),
+                d = new Date,
+                T = [], C = [];
+            T[0] = b[2],            C[0] = d.getFullYear(),
+            T[1] = b[1] - 1,        C[1] = d.getMonth(),
+            T[2] = parseInt(b[0]),  C[2] = d.getDate(),
+
+            T[3] = parseInt(c[0]),  C[3] = d.getHours(),
+            T[4] = parseInt(c[1]),  C[4] = d.getMinutes(),
+            T[5] = parseInt(c[2]),  C[5] = d.getSeconds();
+
+            for (var D = [], j = 0; j < 6; j++)
+                D[j] = C[j] - T[j];
+
+            if (D[5] < 0)
+                D[5] += 60,
+                D[4]--;
+
+            if (D[4] < 0)
+                D[4] += 60,
+                D[3]--;
+
+            if (D[3] < 0)
+                D[3] += 24,
+                D[2]--;
+
+            if (D[2] < 0)
+                D[2] = C[2] + new Date(C[0], C[1], 0).getDate() - T[2],
+                D[1]--;
+
+            if (D[1] < 0)
+                D[1] = C[1] + 12 - T[1],
+                D[0]--;
+
+            for (j = 0; j < 6; j++)
+                formTxt(D[j], j);
+
+            function formTxt(n, q) {
+                var w = [['год', 'года', 'лет'],
+                         ['месяц', 'месяца', 'месяцев'],
+                         ['день', 'дня', 'дней'],
+                         ['час', 'часа', 'часов'],
+                         ['минута', 'минуты', 'минут'],
+                         ['секунда', 'секунды', 'секунд']],
+                    k = n % 10,
+                    l = (!k || n > 5 && n < 21 || k > 4) ? 2 : ((k == 1) ? 0 : 1),
+                    t = w[q][l];
+                $('#count-'+q).text(n+' '+t);
+            }
+            TMR = 1000 * 60;
+        }, TMR);
+
+
         /* Hero height for full and half screen
         ==================================================================================== */
         var windowHeight = $(window).height();
